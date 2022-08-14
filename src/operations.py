c2e342ed9ca0ac70b14b84895b21f3cc018f919d
Zubair <zubairtusar17@gmail.com>
2022-08-14 23:29:27 +0600

    refactor: updated documentation

--- src/operations.py
+++ src/operations.py
@@ -1,10 +1,3 @@
-# import schemas, utils
-
-# def create_user(user: schemas.UserBase):
-#     hashed_password = utils.get_hashed_password(plain_text_password=user.password)
-#     print(f'[LOG] New user: {user.user_name} created with email: {user.email} and password {hashed_password}')
-#     return hashed_password
-
 from sqlalchemy.orm import Session
 import models, schemas, utils
 
